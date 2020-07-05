5d3389c92d89efd5c729cf4e0728e894bb6000ae
Niklas <desk@niklassonnenschein.de>
2020-07-05 22:18:29 +0200

    hm maybe in black?

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,7 +5,7 @@ import ProgressBar from "react-scroll-progress-bar";
 export default function Layout({breadcrumb, children}) {
     return (
         <>
-            <ProgressBar bgcolor="#f4ea66" height="2px"/>
+            <ProgressBar bgcolor="#333" height="1px"/>
             <div className="min-h-screen">
                 <Header breadcrumb={breadcrumb} />
                 <main>{children}</main>
