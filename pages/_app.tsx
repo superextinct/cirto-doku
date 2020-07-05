e16d7247c016e6f53047ab2336c6090f76596505
Niklas <desk@niklassonnenschein.de>
2020-07-05 17:11:51 +0200

    code cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,39 +4,6 @@ import "react-h5-audio-player/lib/styles.css";
 import React, { useEffect } from "react";
 import sal from "sal.js";
 
-// export const useIntersect = ({ root = null, rootMargin, threshold = 0}) => {
-//   const [entry, updateEntry] = React.useState({});
-//   const [node, setNode] = React.useState(null);
-
-//   const observer = React.useRef(null);
-
-//   React.useEffect(
-//     () => {
-//       if (observer.current) observer.current.disconnect();
-
-//       observer.current = new window.IntersectionObserver(
-//         ([entry]) => updateEntry(entry),
-//         {
-//           root,
-//           rootMargin,
-//           threshold
-//         }
-//       );
-
-//       const { current: currentObserver} = observer;
-
-//       if (node) currentObserver.observe(node);
-
-//       return () => currentObserver.disconnect();
-//     },
-//     [node, root, rootMargin, threshold]
-//   );
-
-//   return [setNode, entry];
-// };
-
-
-
 export default function App({ Component, pageProps }) {
   React.useEffect(() => {
     document.querySelectorAll(".notion-h1").forEach( (heading) => {
