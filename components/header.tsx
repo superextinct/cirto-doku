16c91a7ba7fba3169bc3ecfe2cd061908e472f81
Niklas <desk@niklassonnenschein.de>
2020-07-27 14:57:49 +0200

    header hotfix

--- components/header.tsx
+++ components/header.tsx
@@ -25,7 +25,9 @@ const Header = (props?: any) => {
             <div className="flex bg-white fixed top-0 inset-x-0 z-50 h-24 items-center">
                 <div className="container lg:w-10/12 xl:w-4/5 mx-auto px-5 relative mx-auto px-5">
                     <Link href="/">
-                        <a className="logo text-xl md:text-2xl font-normal leading-tight">{DOC_TITLE}</a>
+                        <a className="logo">
+                            <img src="/assets/cirto.svg" alt="cirtō" />
+                        </a>
                     </Link>
                     {breadcrumb}
                 </div>
